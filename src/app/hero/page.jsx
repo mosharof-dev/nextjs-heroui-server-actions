1aff5fd4affe084d36132a99cd30e2e7368b4593
programmermosharof <mosharof.dev@gmail.com>
2026-04-20 01:31:22 +0600

    feat: integrate Hero UI components and update styles
    - Added @heroui/react and @heroui/styles dependencies to package.json.
    - Replaced custom CSS variables and styles in globals.css with @heroui/styles import.
    - Created a new HeroPage component in src/app/hero/page.jsx using Hero UI's Button.
    - Updated the main Home component in src/app/page.js to include the HeroPage and modified the layout.

---
+++ src/app/hero/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@heroui/react";
+
+
+const HeroPage = () => {
+    return (
+        <div>
+            <h2>Hero Page</h2>
+            <Button variant="danger">Hero</Button>
+        </div>
+    );
+};
+
+export default HeroPage;
